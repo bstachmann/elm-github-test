fe2b0ae41c5398ef9f17599d0a53313fbcc7b29e
bstachmann <egal>
2017-10-06 00:29:28 +0200

    Use elm-bootstrap library in Main.elm

--- Main.elm
+++ Main.elm
@@ -1,5 +1,17 @@
 module Main exposing (main)
 
 import Html exposing (..)
+import Bootstrap.Grid as Grid
 
-main = div [] [ text "Moin Moin!" ]
+
+main =
+    Grid.container []
+        [ Grid.row []
+            [ Grid.col [] [ text "Moin Moin"]
+            , Grid.col [] [ text "Hallo Hallo"]
+            ]
+          , Grid.row []
+              [ Grid.col [] [ text "Tach"]
+              , Grid.col [] [ text "auch"]
+          ]
+        ]

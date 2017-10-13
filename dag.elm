f90e9c44fdf1a6b119f392f5137a0ce60557af35
bstachmann <egal>
2017-10-14 01:29:12 +0200

    Huebsch up.

--- dag.elm
+++ dag.elm
@@ -1,10 +1,10 @@
 module Main exposing (main)
 
-import Array exposing (Array, length, push, fromList)
+import Dict exposing (Dict, get, values)
 import Html exposing (Html)
-import List exposing (foldl, map)
+import List exposing (foldl, intersperse, map)
 import Maybe
-import Set exposing (Set, filter, insert, member, remove, singleton)
+import Set exposing (Set, filter, member, remove)
 
 
 type Msg
@@ -14,108 +14,62 @@ type Msg
 main : Html Msg
 main =
     let
-        a =
-            buildSingleton "A"
-
-        b =
-            buildNode "B" [ a ] a
-
-        c =
-            buildNode "C" [ b ] b
-
-        d =
-            buildNode "D" [ a ] c
-
-        e =
-            buildNode "E" [ c, d ] d
-
-        f =
-            buildNode "F" [ c ] e
+        g =
+            empty identity
+                |> node "A" []
+                |> node "B" [ "A" ]
+                |> node "C" [ "B" ]
+                |> node "D" [ "A" ]
+                |> node "E" [ "C", "D" ]
+                |> node "F" [ "C" ]
     in
-        build f
-            |> mapNodes toString
+        mapNodes toString g
             |> map Html.text
-            |> List.intersperse (Html.br [] [])
+            |> intersperse (Html.br [] [])
             |> Html.body []
 
 
-type DagBuilder payload
-    = NewDagBuilder (Node payload) (Dag payload)
+type Dag comparable payload
+    = NewDag (payload -> comparable) (Dict comparable (Node comparable payload)) (Set comparable)
 
 
-type Dag payload
-    = NewDag (Array (Node payload)) (Set Int)
+type Node comparable payload
+    = NewNode payload (List (Node comparable payload))
 
 
-type Node payload
-    = NewNode Int payload (List (Node payload))
+empty : (payload -> comparable) -> Dag comparable payload
+empty getId =
+    NewDag getId Dict.empty Set.empty
 
 
-empty : Dag payload
-empty =
-    NewDag Array.empty Set.empty
-
-
-buildSingleton : payload -> DagBuilder payload
-buildSingleton payload =
+node : payload -> List comparable -> Dag comparable payload -> Dag comparable payload
+node payload successorIds (NewDag getId dagNodes rootIds) =
     let
-        rootNode =
-            NewNode 0 payload []
+        idOfNewNode =
+            getId payload
 
-        nodes =
-            fromList [ rootNode ]
+        newRootIds =
+            foldl remove rootIds successorIds
+                |> Set.insert idOfNewNode
 
-        rootIndexes =
-            singleton 0
-    in
-        NewDagBuilder rootNode (NewDag nodes rootIndexes)
-
-
-buildNode : payload -> List (DagBuilder payload) -> DagBuilder payload -> DagBuilder payload
-buildNode payload predecessorBuilders (NewDagBuilder _ (NewDag dagNodes rootIndexes)) =
-    let
-        indexOfNewNode =
-            length dagNodes
-
-        predecessors =
-            map latest predecessorBuilders
-
-        predecessorIndexes =
-            map indexOf predecessors
-
-        newRootIndexes =
-            foldl remove rootIndexes predecessorIndexes
-                |> insert indexOfNewNode
+        {--TODO suppresses information about missing keys use Result instead of filtering --}
+        successorNodes =
+            List.filterMap (\i -> get i dagNodes) successorIds
 
         newNode =
-            NewNode indexOfNewNode payload predecessors
+            NewNode payload successorNodes
 
         newNodes =
-            push newNode dagNodes
+            Dict.insert idOfNewNode newNode dagNodes
     in
-        NewDagBuilder newNode (NewDag newNodes newRootIndexes)
+        NewDag getId newNodes newRootIds
 
 
-indexOf : Node payload -> Int
-indexOf (NewNode index _ _) =
-    index
-
-
-payload : Node payload -> payload
-payload (NewNode _ p _) =
+payload : Node comparable payload -> payload
+payload (NewNode p _) =
     p
 
 
-latest : DagBuilder payload -> Node payload
-latest (NewDagBuilder latest _) =
-    latest
-
-
-build : DagBuilder payload -> Dag payload
-build (NewDagBuilder _ dag) =
-    dag
-
-
-mapNodes : (Node a -> b) -> Dag a -> List b
-mapNodes function (NewDag dagNodes rootIndexes) =
-    Array.map function dagNodes |> Array.toList
+mapNodes : (Node comparable payload -> a) -> Dag comparable payload -> List a
+mapNodes function (NewDag _ dagNodes _) =
+    values dagNodes |> map function

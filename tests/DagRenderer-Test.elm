8a218648de62474278627f826c6d8e0fa51df1e4
bstachmann <egal>
2017-10-31 21:33:29 +0100

    add some test skeletons

---
+++ tests/DagRenderer-Test.elm
@@ -0,0 +1,22 @@
+module DagRendererTest exposing (..)
+
+import Expect
+import Fuzz exposing (..)
+import Test exposing (..)
+import Test.Runner.Html
+
+
+main : Test.Runner.Html.TestProgram
+main =
+    [ testSomeThing
+    ]
+        |> concat
+        |> Test.Runner.Html.run
+
+
+testSomeThing : Test
+testSomeThing =
+    describe "Some Thing"
+        [ test "this should succeed" <|
+                \() -> Expect.equal "blah" "blah"
+        ]

7ad8372221708285848d8aca1f5cf815a787247d
bstachmann <bstachmann@yahoo.de>
2017-11-10 23:07:59 +0100

    Clean up: Remove some stuff.

--- src/DagManipulationUI.elm
+++ src/DagManipulationUI.elm
@@ -84,7 +84,6 @@ flowGraphCard i graphAsHtml =
         [ class "card" ]
         [ div
             [ class "card-header"
-            , attribute "role" "tab"
             , id "headingOne"
             ]
             [ h5
@@ -101,24 +100,13 @@ flowGraphCard i graphAsHtml =
         , div
             [ id <| "collapse" ++ (toString i)
             , class "collapse show"
-            , attribute "role" "tabpanel"
             , attribute "aria-labelledby" "headingOne"
-            , attribute "data-parent" "#accordion"
             ]
             [ div
                 [ class "card-body" ]
                 graphAsHtml
             ]
         ]
-
-
-
---   <div id="collapseOne" class="collapse show" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion">
---     <div class="card-body">
---       Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
---     </div>
---   </div>
--- </div>
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
